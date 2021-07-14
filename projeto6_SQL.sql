21513cb68fa30afc51a8cba6d8a2a044060188f1
Luciana Aguiar Corrêa <lucianaaguiarc@gmail.com>
2021-07-14 20:06:30 -0300

    Update projeto6_SQL.sql

--- projeto6_SQL.sql
+++ projeto6_SQL.sql
@@ -9,24 +9,30 @@ CREATE TABLE TB_armazens (
 	codArmazem INT NOT NULL PRIMARY KEY,
 	localizacao VARCHAR(255) NOT NULL ,
 	capacidade INTEGER NOT NULL,
-);CREATE TABLE tb_caixas (
+);
+
+CREATE TABLE tb_caixas (
 	codCaixa CHAR(4) NOT NULL PRIMARY KEY,
 	conteudo VARCHAR(255) NOT NULL ,
 	valor REAL NOT NULL ,
 	FK_armazem INT NOT NULL FOREIGN KEY REFERENCES TB_armazens (codArmazem),
-);
-
+);
+
+
+
 --################################################################################################################
 --ETAPA 2: POPULAR A TABELA
 --################################################################################################################
 
---Popular a tabela tb_filmes
+--Popular a tabela TB_armazens
 INSERT INTO TB_armazens (codArmazem,Localizacao,Capacidade) VALUES(1,'Chicago',3);
 INSERT INTO TB_armazens (codArmazem,Localizacao,Capacidade) VALUES(2,'Miami',4);
 INSERT INTO TB_armazens (codArmazem,Localizacao,Capacidade) VALUES(3,'New York',7);
 INSERT INTO TB_armazens (codArmazem,Localizacao,Capacidade) VALUES(4,'Los Angeles',2);
-INSERT INTO TB_armazens (codArmazem,Localizacao,Capacidade) VALUES(5,'San Francisco',8);
---Popular a tabela tb_cinemas
+INSERT INTO TB_armazens (codArmazem,Localizacao,Capacidade) VALUES(5,'San Francisco',8);
+
+
+--Popular a tabela tb_caixas
 INSERT INTO tb_caixas(codCaixa,Conteudo,Valor,FK_armazem) VALUES('0MN7','Pedras',180,3);
 INSERT INTO tb_caixas(codCaixa,Conteudo,Valor,FK_armazem) VALUES('4H8P','Pedras',250,1);
 INSERT INTO tb_caixas(codCaixa,Conteudo,Valor,FK_armazem) VALUES('4RT3','Tesouras',190,4);
@@ -37,7 +43,8 @@ INSERT INTO tb_caixas(codCaixa,Conteudo,Valor,FK_armazem) VALUES('9J6F','Papeis'
 INSERT INTO tb_caixas(codCaixa,Conteudo,Valor,FK_armazem) VALUES('LL08','Pedras',140,4);
 INSERT INTO tb_caixas(codCaixa,Conteudo,Valor,FK_armazem) VALUES('P0H6','Tesouras',125,1);
 INSERT INTO tb_caixas(codCaixa,Conteudo,Valor,FK_armazem) VALUES('P2T6','Tesouras',150,2);
-INSERT INTO tb_caixas(codCaixa,Conteudo,Valor,FK_armazem) VALUES('TU55','Papeis',90,5);
+INSERT INTO tb_caixas(codCaixa,Conteudo,Valor,FK_armazem) VALUES('TU55','Papeis',90,5);
+
 
 
 --################################################################################################################
@@ -45,10 +52,10 @@ INSERT INTO tb_caixas(codCaixa,Conteudo,Valor,FK_armazem) VALUES('TU55','Papeis'
 --################################################################################################################
 
 -------------------------------------
--------Treino seleção e filtro-------
+-------Treino seleÃ§Ã£o e filtro-------
 -------------------------------------
 
---1. Selecionar todos os Armazéns.
+--1. Selecionar todos os ArmazÃ©ns.
 SELECT *
 FROM TB_armazens
 
@@ -57,26 +64,26 @@ SELECT *
 FROM tb_caixas
 WHERE valor > 150
 
---3. Selecionar todos os Conteúdo distintos em todas as Caixas.
+--3. Selecionar todos os ConteÃºdo distintos em todas as Caixas.
 SELECT DISTINCT (conteudo)
 FROM tb_caixas
 
---4. Selecionar o valor médio de todas as Caixas.
+--4. Selecionar o valor mÃ©dio de todas as Caixas.
 SELECT AVG (valor) AS mediaValorCaixa
 FROM tb_caixas
 
---5. Selecionar o armazém Codigo e o valor médio das Caixas em cada armazém.
+--5. Selecionar o armazÃ©m Codigo e o valor mÃ©dio das Caixas em cada armazÃ©m.
 SELECT FK_armazem AS codArmazem, AVG(valor) AS mediaValorCaixa
 FROM tb_caixas
 GROUP BY FK_armazem
 
---6. Selecione apenas aqueles Armazens onde o valor médio das Caixas seja superior a --150.
+--6. Selecione apenas aqueles Armazens onde o valor mÃ©dio das Caixas seja superior a --150.
 SELECT FK_armazem AS codArmazem, AVG(valor) AS mediaValorCaixa
 FROM tb_caixas
 GROUP BY FK_armazem
 HAVING AVG(valor) > 150
 
---7. Selecione o Codigo de cada caixa, junto com o Nome da cidade em que a caixa está --localizada.
+--7. Selecione o Codigo de cada caixa, junto com o Nome da cidade em que a caixa estÃ¡ --localizada.
 SELECT c.codCaixa, a.localizacao
 FROM tb_caixas AS c
 LEFT JOIN TB_armazens as a
@@ -89,12 +96,12 @@ LEFT JOIN TB_armazens as a
 ON FK_armazem = codArmazem
 WHERE a.localizacao = 'Chicago'
 
---9. Selecione o armazém Codigos, juntamente com o número de Caixas em cada armazém.
+--9. Selecione o armazÃ©m Codigos, juntamente com o nÃºmero de Caixas em cada armazÃ©m.
 SELECT FK_armazem AS codArmazem, COUNT(codCaixa) AS totalCaixas
 FROM tb_caixas
 GROUP BY FK_armazem
 
---10. Selecionar os Codigos de todos os Armazéns que estão saturados (um armazém fica saturado se o número de Caixas nele contido for superior à capacidade do armazém).
+--10. Selecionar os Codigos de todos os ArmazÃ©ns que estÃ£o saturados (um armazÃ©m fica saturado se o nÃºmero de Caixas nele contido for superior Ã  capacidade do armazÃ©m).
 SELECT FK_armazem AS codArmazem, COUNT(codCaixa) AS totalCaixas, a.capacidade
 FROM tb_caixas AS t
 LEFT JOIN TB_armazens AS a
@@ -106,15 +113,16 @@ HAVING COUNT(codCaixa) > A.capacidade
 -------Treino coluna cauculada-------
 -------------------------------------
 
---1. Crie uma nova coluna com o nome de ‘Saturacao’. Se a quantidade de Caixas for superior a capacidade que o armazem possui, essa nova coluna terá o valor de “Saturado”, senão terá o valor de “Não Saturado”.
+--1. Crie uma nova coluna com o nome de â€˜Saturacaoâ€™. Se a quantidade de Caixas for superior a capacidade que o armazem possui, essa nova coluna terÃ¡ o valor de â€œSaturadoâ€, senÃ£o terÃ¡ o valor de â€œNÃ£o Saturadoâ€.
 SELECT FK_armazem AS codArmazem, COUNT(codCaixa) AS totalCaixas, a.capacidade,
 CASE
 	WHEN COUNT(codCaixa) > A.capacidade THEN 'Saturado'
-	ELSE 'Não Saturado'
+	ELSE 'NÃ£o Saturado'
 END AS statusDeCapacidade
 FROM tb_caixas AS t
 LEFT JOIN TB_armazens AS a
 ON FK_armazem = codArmazem
 GROUP BY FK_armazem, capacidade
-
+
+
 
